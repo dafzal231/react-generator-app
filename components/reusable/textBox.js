91cc9605464611a8f87cdd82816a5f42e9eede58
Manal Afzal <manal.afzal@ontariotechu.net>
2023-09-15 23:49:32 -0400

    organized, added some folders

---
+++ components/reusable/textBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const textBox = () => {
+    return(
+        <header>
+            <div className = "title"> Title </div>
+            <div className = "text"> </div>
+        </header>
+    )
+}
+
---
+++ constants/type.js
@@ -0,0 +1,18 @@
+// // Components 
+// import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
+// import {useState} from "react";
+
+// const buttonType = [type, setType] = 
+//     useState(initialType);
+
+// function buttonType() 
+// {
+//     return(
+//     <ButtonComponent>
+//       <input
+//         type={type}
+//         placeholder="Default Button"
+//       />
+//     </ButtonComponent>
+//     )
+// }
