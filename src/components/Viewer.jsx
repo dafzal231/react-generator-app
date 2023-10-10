bc80de9fb43240a3f75655f96bcace347784e528
Manal Afzal <manal.afzal@ontariotechu.net>
2023-10-10 17:10:30 -0400

    css

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../styles/globals.css";
-import computer from '../images/computer.png';
-import phone from '../images/phone.png';
-import ipad from '../images/ipad.png';
-import code from '../images/preview.png';
-import sizing from '../images/sizing.png';
+import collaborate from "../images/collaborate.png";
+import customize from "../images/customize.png";
+import preview from "../images/preview.png";
+import sizing from "../images/sizing.png";
+import view_code from "../images/view_code.png";
+
 import { Button } from "antd";
 import { useSelector } from "react-redux";
 
@@ -14,40 +15,28 @@ const Viewer = () => {
 
     console.log(size)
 
-
     return (
         <div className="viewer">
             <div>
-                <div className = "images-1">
                 <div className = "img"> 
-                <img class="img" src={computer} alt="Icon"/>
+                <img class = "img" src={collaborate} alt="Icon"/>
                 </div>
                 <div className = "img" > 
-                <img class="img" src={phone} alt="Icon"/>
+                <img  src={customize} alt="Icon"/>
                 </div>
 
                 <div className = "img"> 
-                <img class="img" src={ipad} alt="Icon"/>
+                <img  src={preview} alt="Icon"/>
                 </div>
-            </div>
-
-        { /*  Will need to make this a number form 
-        ( if the user wants to change it
-            */}
             
-            <div class = "images-2">
-            <div className = "img"> 
-                <img class="img" src={sizing} alt="Icon"/>
+                <img src={sizing} alt="Icon"/>
                 </div> 
 
                 <div className = "img">
-                <img class="img" src={code} alt="Icon"/>
+                <img  src={view_code} alt="Icon"/>
                 </div> 
-            </div>
             <Button disabled={disabled} danger={danger} loading={loading} ghost={ghost} size={size} shape={shape}>Click Me</Button>
         </div>
-      </div>
-
     )
 }
 
