82445faf99dbf0718d960ce7b7e84feae5ca4768
Manal Afzal <manal.afzal@ontariotechu.net>
2023-10-10 19:46:48 -0400

    homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import DropDown from "./components/reusable/dropDown";
 import Nav from "./components/Nav.jsx";
 import Generator from "./components/Generator.jsx";
+import Login from "./components/Login.jsx";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <div className="app">
-      <Nav></Nav>
-      <Generator></Generator>
-    </div>
-    </>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/generator" element={<Generator />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div className = "background">
+            <div className = "container">
+                <div className = "company-title">
+                    <h1>Company</h1>
+                </div>
+
+                <div className = "description">
+                    <p className = "paragraph"> Transform your React UI with our versatile button design generator. 
+                     Craft unique and visually appealing buttons tailored to your style effortlessly.  
+                     Explore endless customization options, making every click on your UI a delightful 
+                     experience for users.
+                     </p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Login;
+
