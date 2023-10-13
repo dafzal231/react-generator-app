97f661c842ecde3e6cd23db9a81049618f6efd9f
Manal Afzal <manal.afzal@ontariotechu.net>
2023-10-13 16:37:14 -0400

    update login.js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-
 const Login = () => {
     return (
         <div className = "background">
-            <div className = "container">
+            <div className = "login-container">
                 <div className = "company-title">
                     <h1>Company</h1>
                 </div>
 
                 <div className = "description">
-                    <p className = "paragraph"> Transform your React UI with our versatile button design generator. 
-                     Craft unique and visually appealing buttons tailored to your style effortlessly.  
-                     Explore endless customization options, making every click on your UI a delightful 
-                     experience for users.
-                     </p>
+                    <div className = "paragraph">
+                        Transform your React UI with our versatile button design generator. 
+                        Craft unique and visually appealing buttons tailored to your style 
+                        effortlessly.  Explore endless customization options, making every 
+                        click on your UI a delightful experience for users.
+                    </div>
+                    <button className = "trynow-btn">TRY NOW</button>
                 </div>
-
             </div>
         </div>
     )
